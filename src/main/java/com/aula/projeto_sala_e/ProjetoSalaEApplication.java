2583cc01f6ebd7f1ec826df91854c1702be0d3fd
Nisflei <ncgaloni@hotmail.com>
2023-08-29 18:03:33 -0300

    first commit

---
+++ src/main/java/com/aula/projeto_sala_e/ProjetoSalaEApplication.java
@@ -0,0 +1,13 @@
+package com.aula.projeto_sala_e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSalaEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSalaEApplication.class, args);
+	}
+
+}
