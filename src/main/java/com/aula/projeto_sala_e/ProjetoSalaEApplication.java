9593136cf11b78bec4b9581e219dff3f247badba
Nisflei <ncgaloni@hotmail.com>
2023-08-29 18:04:40 -0300

    socorro

--- src/main/java/com/aula/projeto_sala_e/ProjetoSalaEApplication.java
+++ src/main/java/com/aula/projeto_sala_e/ProjetoSalaEApplication.java
@@ -2,12 +2,19 @@ package com.aula.projeto_sala_e;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProjetoSalaEApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoSalaEApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSalaEApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String oi(){
+        return "Manu cuida da turma... Germinare Tech..!";
+    }
 }
--- src/test/java/com/aula/projeto_sala_e/ProjetoSalaEApplicationTests.java
+++ src/test/java/com/aula/projeto_sala_e/ProjetoSalaEApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjetoSalaEApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
